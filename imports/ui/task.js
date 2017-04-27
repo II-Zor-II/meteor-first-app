04e6ff2e56370835373d47ccc4baef05b790ae69
Zor <winzorJMpaelmo@gmail.com>
2017-04-27 11:00:59 +0800

    changed into secure login

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
@@ -7,12 +8,14 @@ import './task.html';
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
+/*    Tasks.update(this._id, {
       $set: { checked: ! this.checked },
-    });
+    });*/
+   Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   'click .delete'() { //handles the even for .delete button, and well
-    Tasks.remove(this._id); // remove it from the Tasks.collection
+//    Tasks.remove(this._id); // remove it from the Tasks.collection
+   Meteor.call('tasks.remove', this._id, !this.checked);
    /*
     In a collection, every inserted document has a unique _id field that can be used to refer to that specific document. 
    */
